74e9b71ba3a972dedfd04da2e3a3b58402658526
lijingbo <906139088@qq.com>
2020-11-23 10:01:33 +0800

    关闭eslint

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,6 +9,13 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          change thing
+          
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
